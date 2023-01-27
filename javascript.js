22c981dd5bb51a864cdd5246c02f25ea43b14b24
eclavel <113940257+eclavel@users.noreply.github.com>
2023-01-27 03:16:39 -0800

    playRound Function was added and works

--- javascript.js
+++ javascript.js
@@ -3,5 +3,42 @@ function getComputerChoice(){
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
-
-console.log(getComputerChoice());
+function playRound(playerSelection, computerChoice){
+    console.log(computerChoice);
+    console.log(playerSelection);
+    if(playerSelection == 'Rock'){
+        if(computerChoice == 'Paper'){
+            return 'You Lose! Paper beats Rock';
+        }
+        else if(computerChoice == 'Scissor'){
+            return 'You Win! Rock beats Scissor';
+        }
+        else{
+            return 'Tie!';
+        }
+    }
+    else if(playerSelection == 'Paper'){
+        if(computerChoice == 'Rock'){
+            return 'You Win! Paper beats Rock';
+        }
+        else if(computerChoice == 'Scissor'){
+            return 'You Lose! Scissor beats Paper';
+        }
+        else{
+            return 'Tie!';
+        }
+    }
+    else if(playerSelection == 'Scissor'){
+        if(computerChoice == 'Rock'){
+            return 'You Lose! Rock beats Scissor';
+        }
+        else if(computerChoice == 'Paper'){
+            return 'You win! Scissor beats Paper';
+        }
+        else{
+            return 'Tie!';
+        }
+    }
+}
+let playerSelection = prompt('Choose: Rock, Paper, or Scissor','Rock');
+console.log(playRound(playerSelection,getComputerChoice()));
